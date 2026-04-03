15f08be1692f10cbe51093c050f0243732ac1d02
Attila Albert <s_a_albert@yahoo.com>
2026-04-03 20:04:26 +0000

    1st pipeline

---
+++ pipeline/pipeline.py
@@ -0,0 +1,14 @@
+import sys
+import pandas as pd
+
+print('arguments', sys.argv)
+
+month = int(sys.argv[1]) if len(sys.argv) > 1 else 0
+
+df=pd.DataFrame({"day": [1, 2], "num_passangers": [3, 4]})
+
+df['mont'] = month
+print(df.head())
+df.to_parquet(f"output_{month}.parquet")
+
+print(f'Hello pipeline, month={month}')
